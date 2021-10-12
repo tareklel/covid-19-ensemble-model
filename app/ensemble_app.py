200db3ec989807fc83516d8e83298ecf3479a3e0
Tarek Lel <tareklel@gmail.com>
2021-10-12 09:41:20 +0400

    added model_net.py added classes, needs debugging for running model

--- app/ensemble_app.py
+++ app/ensemble_app.py
@@ -18,9 +18,127 @@ from torch.utils.data import DataLoader, random_split
 from torchvision.utils import make_grid
 import torchvision.models as models
 from torchvision.datasets import ImageFolder
-from mlxtend.plotting import plot_confusion_matrix
-from sklearn.metrics import confusion_matrix
 import albumentations as a
+from model_net import ModelBase, ModelNet
+
+
+DEMO_DIR = 'test_images/normal'
+
+
+class EnsembleModel:
+    def __init__(self):
+        self.model_path = 'models'
+        self.clahe_dir = 'clahe_dir'
+        self.model_dict = {'vgg': 'models/vgg.pth',
+                           'densenet': 'models/densenet.pth',
+                           'resnet': 'models/resnext.pth'
+                          }
+
+    def get_image_list(self, path):
+        """
+        get path of images and return list of image path
+        """
+        self.image_path = path
+        self.image_list = []
+        for impth in os.listdir(self.image_path):
+            path = self.image_path + '/' + impth
+            if os.path.isfile(self.image_path + '/' + impth):
+                self.image_list.append(path)
+
+    def convert_clahe(self):
+        """
+        create clahe folder,  process images with clahe to folder
+        """
+        os.mkdir('clahe_dir')
+        os.mkdir('clahe_dir/clahe_dir')
+        for file in self.image_list:
+            image_name = file.split('/')[-1]
+            img = cv2.imread(file, 0)
+            clahe = cv2.createCLAHE(tileGridSize=(8,8))
+            cl1 = clahe.apply(img)
+            cv2.imwrite('clahe_dir/clahe_dir/' + image_name, cl1)
+
+    def get_default_device(self):
+        """Pick GPU if available, else CPU"""
+        if torch.cuda.is_available():
+            return torch.device('cuda')
+        else:
+            return torch.device('cpu')
+
+    def load_model(self, name):
+        """loads deep learning model"""
+        filepath = self.model_dict[name]
+        checkpoint = torch.load(filepath, self.get_default_device())
+        model = checkpoint['model']
+        model.load_state_dict(checkpoint['state_dict'])
+        for parameter in model.parameters():
+            parameter.requires_grad = False
+        model.eval()
+        print(f'{model.network.__class__.__name__} loaded')
+        return model
+
+    @torch.no_grad()
+    def test_predict(self, model, test_loader):
+        model.eval()
+        # perform testing for each batch
+        outputs = [model.validation_step(batch) for batch in test_loader]
+        results = model.test_prediction(outputs)
+        print('test_loss: {:.4f}, test_acc: {:.4f}'
+              .format(results['test_loss'], results['test_acc']))
+
+        return results['test_preds'], results['test_labels'], results['out']
+
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            # yield will stop here, perform other steps,
+            # and the resumes to the next loop/batch
+            yield to_device(b,
+                            self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+def create_test_dataset(data_dir):
+    test_dataset = ImageFolder(data_dir,
+                               transform=transforms.Compose([
+                                   transforms.Resize(512),
+                                   transforms.CenterCrop(480),
+                                   transforms.ToTensor(),
+                               ]))
+    return test_dataset
+
+
+if __name__ =="__main__":
+    master = EnsembleModel()
+    #master.get_image_list(DEMO_DIR)
+    #master.convert_clahe()
+    test_dataset = create_test_dataset('clahe_dir')
+    test_dl = DataLoader(test_dataset, batch_size=(20))
+    test_dl = DeviceDataLoader(test_dl, master.get_default_device())
+    model = master.load_model('densenet')
+    model(test_dl)
+
+
+
+
+
+
 
 
 
---
+++ app/model_net.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ModelBase(nn.Module):
+
+    # this is for loading the batch of train image and outputting its loss, accuracy
+    # & predictions
+    def training_step(self, batch, weight):
+        images, labels = batch
+        out = self(images)  # generate predictions
+        loss = F.cross_entropy(out, labels, weight=weight)  # weighted compute loss
+        acc, preds = accuracy(out, labels)  # calculate accuracy
+
+        return {'train_loss': loss, 'train_acc': acc}
+
+    # this is for computing the train average loss and acc for each epoch
+    def train_epoch_end(self, outputs):
+        batch_losses = [x['train_loss'] for x in outputs]  # get all the batches loss
+        epoch_loss = torch.stack(batch_losses).mean()  # combine losses
+        batch_accs = [x['train_acc'] for x in outputs]  # get all the batches acc
+        epoch_acc = torch.stack(batch_accs).mean()  # combine accuracies
+
+        return {'train_loss': epoch_loss.item(), 'train_acc': epoch_acc.item()}
+
+    # this is for loading the batch of val/test image and outputting its loss, accuracy,
+    # predictions & labels
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # generate predictions
+        loss = F.cross_entropy(out, labels)  # compute loss
+        acc, preds = accuracy(out, labels)  # calculate acc & get preds
+
+        return {'val_loss': loss.detach(), 'val_acc': acc.detach(),
+                'preds': preds.detach(), 'labels': labels.detach(),
+                'out': out}  # IMPORTANT added out
+
+    # detach extracts only the needed number, or other numbers will crowd memory
+
+    # this is for computing the validation average loss and acc for each epoch
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]  # get all the batches loss
+        epoch_loss = torch.stack(batch_losses).mean()  # combine losses
+        batch_accs = [x['val_acc'] for x in outputs]  # get all the batches acc
+        epoch_acc = torch.stack(batch_accs).mean()  # combine accuracies
+
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    # this is for printing out the results after each epoch
+    def epoch_end(self, epoch, train_result, val_result):
+        print('Epoch [{}], train_loss: {:.4f}, train_acc: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}'.
+              format(epoch + 1, train_result['train_loss'], train_result['train_acc'],
+                     val_result['val_loss'], val_result['val_acc']))
+
+    # this is for using on the test set, it outputs the average loss and acc,
+    # and outputs the predictions
+    def test_prediction(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # combine accuracies
+        # combine predictions
+        batch_preds = [pred for x in outputs for pred in x['preds'].tolist()]
+        # combine labels
+        batch_labels = [lab for x in outputs for lab in x['labels'].tolist()]
+
+        # IMPORTANT added out
+        out = [x['out'] for x in outputs]
+
+        return {'test_loss': epoch_loss.item(), 'test_acc': epoch_acc.item(),
+                'test_preds': batch_preds, 'test_labels': batch_labels, 'out': out}
+
+
+class ModelNet(ModelBase):
+    def __init__(self, network):
+        super().__init__()
+        # Use a pretrained model
+        self.network = network
+        # Freeze training for all layers before classifier
+        # resnet
+        if self.network.__class__.__name__ == 'ResNet':
+            for param in self.network.fc.parameters():
+                param.require_grad = False
+                # vgg16
+        if self.network.__class__.__name__ == 'VGG':
+            for param in self.network.classifier[6].parameters():
+                param.require_grad = False
+        # densenet
+        if self.network.__class__.__name__ == 'DenseNet':
+            for param in self.network.classifier.parameters():
+                param.require_grad = False
+
+    def forward(self, xb):
+        return self.network(xb)
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds)), preds
+
+
+def F1_score(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+
+    # precision, recall, and F1
+    cm = confusion_matrix(labels, preds)
+    tn, fp, fn, tp = cm.ravel()
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * ((precision * recall) / (precision + recall))
+
+    return precision, recall, f1, preds
